934eedf9bd52881c89e3be493fec35a33a5327fe
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-06-22 18:15:58 +0200

    Implemented sky

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Sky } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+
 function App() {
   
 
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Canvas>
+        <Sky sunPosition={[100, 100, 20]}/>
+      </Canvas>
+    </>
   )
 }
 
