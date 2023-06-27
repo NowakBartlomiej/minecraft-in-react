e01d53c8e0a465bcf5f0d9661ab8e77b86406924
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-06-27 14:26:10 +0200

    Implemented state managment and cubes

--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -10,11 +10,9 @@ export const Ground = () => {
     position: [0,0,0]
   }))
 
-  groundTexture.magFilter = NearestFilter;
-  groundTexture.wrapS = RepeatWrapping;
-  groundTexture.wrapT = RepeatWrapping;
   groundTexture.repeat.set(100, 100)
-  
+
+
   return (
     <mesh ref={ref}>
       <planeBufferGeometry attach='geometry' args={[100, 100]}/>
